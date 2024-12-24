31d6de73d34643e721d8a05515ddc6bb774dd511
elorapeter <florenceonyi09@gmail.com>
2024-12-24 20:40:49 +0000

    Dec 24, 2024, 9:40 PM

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// const navbar = document.getElementById("navbar");
-// let timeout;
+const navbar = document.getElementById("navbar");
+let timeout;
 
-// window.addEventListener("scroll", () => {
-//     // Clear any existing timeout
-//     clearTimeout(timeout);
+window.addEventListener("scroll", () => {
+    // Clear any existing timeout
+    clearTimeout(timeout);
 
-//     // Show the navbar
-//     navbar.classList.remove("hidden");
+    // Show the navbar
+    navbar.classList.remove("hidden");
 
-//     // Hide the navbar after 3 seconds of inactivity
-//     timeout = setTimeout(() => {
-//         navbar.classList.add("hidden");
-//     }, 2000);
-// });
+    // Hide the navbar after 3 seconds of inactivity
+    timeout = setTimeout(() => {
+        navbar.classList.add("hidden");
+    }, 2000);
+});
 
 
 
