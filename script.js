6631f6b04b5c6eb398234091fa3c425e3e700bd9
elorapeter <florenceonyi09@gmail.com>
2024-12-26 02:55:00 +0000

    Dec 26, 2024, 3:55 AM

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ window.addEventListener("scroll", () => {
         if (!isScrolling) {
             hideNavbar();
         }
-    }, 2000);
+    }, 1000);
 
     // Update scroll position to determine scroll direction
     lastScrollPosition = window.scrollY;
@@ -65,54 +65,3 @@ document.addEventListener("keydown", () => {
 
 // Initial inactivity timeout
 inactivityTimeout = setTimeout(hideNavbar, 2000);
-
-
-
-
-
-
-
-
-
-
-// const navbar = document.getElementById("navbar");
-// let timeout;
-
-// window.addEventListener("scroll", () => {
-//     // Clear any existing timeout
-//     clearTimeout(timeout);
-
-//     // Show the navbar
-//     navbar.classList.remove("hidden");
-
-//     // Hide the navbar after 3 seconds of inactivity
-//     timeout = setTimeout(() => {
-//         navbar.classList.add("hidden");
-//     }, 2000);
-// });
-
-
-
-
-// const navbar = document.getElementById("navbar");
-// let inactivityTimeout;
-
-// // Function to hide the navbar
-// const hideNavbar = () => {
-//     navbar.classList.add("hidden");
-// };
-
-// // Event listener for scroll events
-// window.addEventListener("scroll", () => {
-//     // Show the navbar when scrolling
-//     navbar.classList.remove("hidden");
-
-//     // Clear the previous timeout
-//     clearTimeout(inactivityTimeout);
-
-//     // Set a new timeout to hide the navbar after 2 seconds
-//     inactivityTimeout = setTimeout(hideNavbar, 2000);
-// });
-
-// // Initial timeout to hide the navbar after 2 seconds of inactivity
-// inactivityTimeout = setTimeout(hideNavbar, 2000);
